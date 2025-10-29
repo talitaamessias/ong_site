37683746d105a145abc07c67acfc2acd07f0212a
Talita Messias <taliresenha@gmail.com>
2025-10-29 09:50:04 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+function maskCPF(value){return value.replace(/\D/g,'').replace(/(\d{3})(\d)/,'$1.$2').replace(/(\d{3})(\d)/,'$1.$2').replace(/(\d{3})(\d{1,2})$/,'$1-$2');}
+function maskPhone(value){return value.replace(/\D/g,'').replace(/(\d{2})(\d)/,'($1) $2').replace(/(\d{5})(\d{4})$/,'$1-$2');}
+function maskCEP(value){return value.replace(/\D/g,'').replace(/(\d{5})(\d{3})$/,'$1-$2');}
+
+document.addEventListener("DOMContentLoaded",()=>{
+  const cpf=document.querySelector("#cpf");
+  const phone=document.querySelector("#telefone");
+  const cep=document.querySelector("#cep");
+  if(cpf) cpf.addEventListener("input",(e)=>e.target.value=maskCPF(e.target.value));
+  if(phone) phone.addEventListener("input",(e)=>e.target.value=maskPhone(e.target.value));
+  if(cep) cep.addEventListener("input",(e)=>e.target.value=maskCEP(e.target.value));
+
+  const form=document.querySelector("form#cadastro");
+  if(form){
+    form.addEventListener("submit",(e)=>{
+      if(!form.checkValidity()){
+        e.preventDefault();
+        alert("Por favor, complete os campos obrigatórios no formato correto.");
+      }
+    });
+  }
+});
