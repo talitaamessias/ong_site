3dd79799c595b88f301997fd1189b85077d21c01
Talita Messias <taliresenha@gmail.com>
2025-11-03 01:01:35 -0500

    Update templates.cadastro.js

--- assets/js/modules/templates.cadastro.js
+++ assets/js/modules/templates.cadastro.js
@@ -13,15 +13,15 @@ export function CadastroTemplate(){
       <fieldset>
         <legend><strong>Dados Pessoais</strong></legend>
         <div class="grid grid-2">
-          <div><label>Nome Completo</label><input id="nome" required placeholder="Seu nome completo"><small class="invalid-msg" data-error-for="nome"></small></div>
-          <div><label>E-mail</label><input id="email" type="email" required placeholder="voce@exemplo.com"><small class="invalid-msg" data-error-for="email"></small></div>
-          <div><label>CPF</label><input id="cpf" required placeholder="000.000.000-00"><small class="small">Formato: 000.000.000-00</small><small class="invalid-msg" data-error-for="cpf"></small></div>
-          <div><label>Telefone</label><input id="telefone" required placeholder="(00) 00000-0000"><small class="small">Formato: (00) 00000-0000</small><small class="invalid-msg" data-error-for="telefone"></small></div>
-          <div><label>Data de Nascimento</label><input id="nascimento" type="date" required><small class="invalid-msg" data-error-for="nascimento"></small></div>
-          <div><label>CEP</label><input id="cep" required placeholder="00000-000"><small class="invalid-msg" data-error-for="cep"></small></div>
-          <div><label>Endereço</label><input id="endereco" required placeholder="Rua, número, complemento"><small class="invalid-msg" data-error-for="endereco"></small></div>
-          <div><label>Cidade</label><input id="cidade" required><small class="invalid-msg" data-error-for="cidade"></small></div>
-          <div><label>Estado</label><input id="uf" required placeholder="SP, RJ, MG…"><small class="invalid-msg" data-error-for="uf"></small></div>
+          <div><label for="nome">Nome Completo</label><input id="nome" required placeholder="Seu nome completo"><small class="invalid-msg" data-error-for="nome"></small></div>
+          <div><label for="email">E-mail</label><input id="email" type="email" required placeholder="voce@exemplo.com"><small class="invalid-msg" data-error-for="email"></small></div>
+          <div><label for="cpf">CPF</label><input id="cpf" required placeholder="000.000.000-00"><small class="small">Formato: 000.000.000-00</small><small class="invalid-msg" data-error-for="cpf"></small></div>
+          <div><label for="telefone">Telefone</label><input id="telefone" required placeholder="(00) 00000-0000"><small class="small">Formato: (00) 00000-0000</small><small class="invalid-msg" data-error-for="telefone"></small></div>
+          <div><label for="nascimento">Data de Nascimento</label><input id="nascimento" type="date" required><small class="invalid-msg" data-error-for="nascimento"></small></div>
+          <div><label for="cep">CEP</label><input id="cep" required placeholder="00000-000"><small class="invalid-msg" data-error-for="cep"></small></div>
+          <div><label for="endereco">Endereço</label><input id="endereco" required placeholder="Rua, número, complemento"><small class="invalid-msg" data-error-for="endereco"></small></div>
+          <div><label for="cidade">Cidade</label><input id="cidade" required><small class="invalid-msg" data-error-for="cidade"></small></div>
+          <div><label for="uf">Estado</label><input id="uf" required placeholder="SP, RJ, MG…"><small class="invalid-msg" data-error-for="uf"></small></div>
           <div><label><input id="termos" type="checkbox"> Confirmo a veracidade das informações.</label><small class="invalid-msg" data-error-for="termos"></small></div>
         </div>
       </fieldset>
